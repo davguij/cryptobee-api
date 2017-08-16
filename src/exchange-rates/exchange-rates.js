a6842ef7cb8dfad7cec594dc7bbbf18385d1b91b
David Guijarro <guijarro.dav@gmail.com>
2017-08-16 18:43:30 +0200

    New API for some coins.

--- src/exchange-rates/exchange-rates.js
+++ src/exchange-rates/exchange-rates.js
@@ -1,19 +1,28 @@
 const http = require('../common/http');
 
 function getRates(currency) {
-	return http.get(`https://api.uphold.com/v0/ticker/${currency.toUpperCase()}`)
-		.then((rawRates) => {
-			let rates = [];
-			rawRates.forEach(function (rawRate) {
-				let currencyCode = rawRate.pair.replace(currency.toUpperCase(), '');
-				let newRate = {
-					rate: rawRate.bid,
-					currency: currencyCode
-				};
-				rates.push(newRate);
-			}, this);
-			return rates;
+	if (currency === 'btc' || currency === 'ltc' || currency === 'eth') {
+		return http.get(`https://api.uphold.com/v0/ticker/${currency.toUpperCase()}`)
+			.then((rawRates) => {
+				let rates = [];
+				rawRates.forEach(function (rawRate) {
+					let currencyCode = rawRate.pair.replace(currency.toUpperCase(), '');
+					let newRate = {
+						rate: rawRate.bid,
+						currency: currencyCode
+					};
+					rates.push(newRate);
+				}, this);
+				return rates;
+			});
+	} else {
+		return http.get(`https://api.cryptonator.com/api/ticker/${currency}-usd`).then((rate) => {
+			return [{
+				rate: rate.ticker.price,
+				currency: 'USD'
+			}];
 		});
+	}
 }
 
 module.exports = { getRates };
